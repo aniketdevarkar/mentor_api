6223f85dc49b45713d601fbe9fa474b8700edbe1
aniketdevarkar <46354537+aniketdevarkar@users.noreply.github.com>
2021-03-28 01:17:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongodb = require("mongodb");
 
 const mongoClient = mongodb.MongoClient;
 // const dbUrl = "mongodb://127.0.0.1:27017";
-//7a6yOjjMXcxWCRBB
+
 const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017";
 const port = process.env.PORT || 8080;
 let globalIdCount = 1;
